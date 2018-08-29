c00ee4d94daf0cbab691de265ae72f7b197a8b68
lynxz <lynxz8866@gmail.com>
2018-08-29 15:48:24 +0800

    在CMakeList.txt中设置变量,并传入到源代码中

--- HelloJniApplication/app/src/main/cpp/native-lib.cpp
+++ HelloJniApplication/app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include <string>
-#include "config.h"
 #include "AndroidLog.h"
+#include "Config.h"
 
 extern "C" JNIEXPORT jstring
 
@@ -10,6 +10,9 @@ Java_org_lynxz_hellojniapplication_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    // 导入的 Config.h 在 AS 中标红提示错误,但运行时是存在的
     LOGD("version %d %d", VERSION_MAJOR, VERSION_MINOR);
+
     return env->NewStringUTF(hello.c_str());
 }
